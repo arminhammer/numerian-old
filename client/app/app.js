9e341aec5ca0fc71225659f36ff8b3c3e61bd39b
arminhammer <arminhammer@gmail.com>
2015-04-27 22:12:38 -0400

    Moved existing code into the new app structure.

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,9 @@ angular.module('numerianApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'angularFileUpload',
+  'chart.js'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
