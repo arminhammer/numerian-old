5a9940cd910af7ae555a1683783b84554440400c
arminhammer <arminhammer@gmail.com>
2015-04-27 21:55:13 -0400

    Initial commit after switching to angular-fullstack generator.

---
+++ client/components/auth/user.service.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('numerianApp')
+  .factory('User', function ($resource) {
+    return $resource('/api/users/:id/:controller', {
+      id: '@_id'
+    },
+    {
+      changePassword: {
+        method: 'PUT',
+        params: {
+          controller:'password'
+        }
+      },
+      get: {
+        method: 'GET',
+        params: {
+          id:'me'
+        }
+      }
+	  });
+  });
--- src/app/components/navbar/navbar.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('numerian')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
